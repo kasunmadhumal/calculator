d25335f30357195be1e578432271ea71bd7870e4
Kasun-se2018-023 <73273550+Kasun-se-2018-023@users.noreply.github.com>
2021-08-07 17:23:13 +0530

    add programe files for read a numbers.txt file and print out answer considering user input

---
+++ src/java/com/kelani/calreadtextfile/Cal.java
@@ -0,0 +1,11 @@
+package com.kelani.calreadtextfile;
+
+public class Cal {
+
+    public static void main(String[] args){
+        new UserInput();
+        new ReadTextFile();
+        new Calculate(UserInput.choice,ReadTextFile.num);
+    }
+
+}
